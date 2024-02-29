d3956dfdd20d5bc84ea911a355c1f6ee6a358c07
Nitinyada <94897177+Nitinyada@users.noreply.github.com>
2024-02-29 18:31:25 +0530

    Add files via upload

---
+++ guess no (computer).py
@@ -0,0 +1,21 @@
+Welcome to Guess the Number game!
+I have chosen a number between 1 and 100. Can you guess it?
+Enter your guess (between 1 and 100): 
+55
+Too high! Try again.
+Enter your guess (between 1 and 100): 
+15
+Too low! Try again.
+Enter your guess (between 1 and 100): 
+85
+Too high! Try again.
+Enter your guess (between 1 and 100): 
+22
+Too low! Try again.
+Enter your guess (between 1 and 100): 
+30
+Too low! Try again.
+Enter your guess (between 1 and 100): 
+
+Session Killed due to Timeout.
+Press Enter to exit terminal
